50ff838b1119f15bf4035dbf258d5010cec948e4
DavidHuji <nukraidavid@mail.tau.ac.il>
2022-08-18 22:18:29 +0300

    balance fairness

--- parse_flicker.py
+++ parse_flicker.py
@@ -140,6 +140,10 @@ def run_main():
         out_path = f"./data/parsed_coco_snowboarding_split_train.pkl"
         annotations_path = f"/home/gamir/DER-Roei/davidn/CLIP_prefix_caption/coco_snowboarding_annnotations/my_coco_snowboarding_train.json"
         images_path = f'NoImgs'
+    elif args.dataset_mode == 9:
+        out_path = f"./data/BALANCED_parsed_coco_snowboarding_split_train.pkl"
+        annotations_path = f"/home/gamir/DER-Roei/davidn/CLIP_prefix_caption/coco_snowboarding_annnotations/my_coco_snowboarding_train.json"
+        images_path = f'NoImgs'
 
     exit(main(args.clip_model_type, clip_model_name, out_path, annotations_path, images_path, args.fix_gender_imbalance))
 
