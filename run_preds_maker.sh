d03bf775a5702b0a20f1b10f15a62acb81966776
DavidHuji <nukraidavid@mail.tau.ac.il>
2022-08-27 12:28:28 +0300

    review ablation

--- run_preds_maker.sh
+++ run_preds_maker.sh
@@ -1,1 +1,4 @@
 CUDA_VISIBLE_DEVICES=2 python predictions_runner.py  --mapping_type transformer_encoder  --num_layers 8 --prefix_length 40 --prefix_length_clip 40 --checkpoint ../transformer_weights.pt
+
+for ablation distance:
+add --dataset_mode 5 --text_autoencoder --ablation_dist --ablation_dist_review
