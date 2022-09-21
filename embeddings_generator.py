67363a52ae40a918db9ffcfc8b302b7186e8e6db
DavidHuji <nukraidavid@mail.tau.ac.il>
2022-09-21 16:05:36 +0300

    better metrics doc

--- embeddings_generator.py
+++ embeddings_generator.py
@@ -122,6 +122,13 @@ def run_main():
             print(f'Text embeddings will be added to the dataset')
         annotations_path = f'/home/gamir/DER-Roei/davidn/myprivate_coco/annotations/train.json'
         images_path = '/home/gamir/DER-Roei/davidn/myprivate_coco/train2014/'
+    elif args.dataset_mode == 0.5:
+        out_path = f"./data/coco/COCO_val_set.pkl"
+        if add_text_embedding:
+            out_path = f"./data/coco/COCO_val_set_with_text.pkl"
+            print(f'Text embeddings will be added to the dataset')
+        annotations_path = f'/home/gamir/DER-Roei/davidn/myprivate_coco/annotations/val.json'
+        images_path = '/home/gamir/DER-Roei/davidn/myprivate_coco/val2014/'
     elif args.dataset_mode == 1:
         out_path = f"./data/flicker30_{clip_model_name}_train.pkl"
         if add_text_embedding:
