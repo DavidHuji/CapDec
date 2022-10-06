b3c40eb65a80ee142e0995f61b220d0424b590ce
DavidHuji <nukraidavid@mail.tau.ac.il>
2022-10-06 14:05:47 +0300

    bridger

--- supervised_embedding_bridger.py
+++ supervised_embedding_bridger.py
@@ -33,14 +33,38 @@ def get_map_to_text_space_using_modality_bridger():
 def get_dataloader():
     with open('coco_clip_embeddings/oscar_split_RN50x4_train_with_text_embeddings.pkl', 'rb') as f:
         data = pickle.load(f)
-        images_embeddings = data['clip_embedding'][:320 if DBG else -1].float()
+        data['captions'] = data['captions'][:160000 if DBG else -1]
+        data['clip_embedding'] = data['clip_embedding'][:160000 if DBG else -1]
+        data['clip_embedding_text_dave'] = data['clip_embedding_text_dave'][:160000 if DBG else -1]
+        images_embeddings = data['clip_embedding'].float()
         images_embeddings /= torch.norm(images_embeddings, dim=1, keepdim=True)
 
-        text_embeddings = data['clip_embedding_text_dave'][:320 if DBG else -1].float()
+        text_embeddings = data['clip_embedding_text_dave'].float()
         text_embeddings /= torch.norm(text_embeddings, dim=1, keepdim=True)
-    return get_torch_dataloader_from_list(images_embeddings, text_embeddings, batch_size_train=BTCH_SIZE,
-                                   batch_size_test=BTCH_SIZE,
-                                   split=[int(0.8 * images_embeddings.shape[0]), int(0.2 * images_embeddings.shape[0])])
+    # split to test and train by image id
+    print('images_embeddings', images_embeddings.shape)
+    image_ids = [s['image_id'] for s in data['captions']]
+    print('sample...')
+    # test_ids = random.sample(image_ids, int(len(image_ids) * 0.2))
+    test_ids = image_ids[:int(len(image_ids) * 0.2)]
+    print('indexs...')
+    train_ids = image_ids[int(len(image_ids) * 0.2):]
+    # train_embeddings_indexs = [s['clip_embedding'] for s in data['captions'] if s['image_id'] in train_ids]
+    # test_embeddings_indexs = [s['clip_embedding'] for s in data['captions'] if s['image_id'] in test_ids]
+    train_embeddings_indexs = []
+    test_embeddings_indexs = []
+    data['captions'].sort(key=lambda x: x['image_id'])
+    for i, s in enumerate(data['captions']):
+        if i < int(len(data['captions']) * 0.2):
+            test_embeddings_indexs.append(s['clip_embedding'])
+        else:
+            train_embeddings_indexs.append(s['clip_embedding'])
+    print('indexs...')
+    train_images_embeddings = images_embeddings[train_embeddings_indexs]
+    test_images_embeddings = images_embeddings[test_embeddings_indexs]
+    train_text_embeddings = text_embeddings[train_embeddings_indexs]
+    test_text_embeddings = text_embeddings[test_embeddings_indexs]
+    return get_torch_dataloader_from_list(train_images_embeddings, train_text_embeddings, test_images_embeddings, test_text_embeddings, batch_size_train=BTCH_SIZE, batch_size_test=BTCH_SIZE)
 
 
 def get_torch_dataloader_from_list(list_x, list_y, list_x_test=None, list_y_test=None, batch_size_train=1, batch_size_test=1, split=None):
@@ -71,7 +95,9 @@ class MLP(nn.Module):
     def forward(self, x):
         for i, layer in enumerate(self.layers):
             x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
-        return torch.sigmoid(x)
+        x = torch.sigmoid(x) - 0.5 # normalize to [-0.5, 0.5]
+        # x /= torch.norm(x, dim=1, keepdim=True)
+        return x
 
 
 def noise_augmentation(x, variance=0.001):
@@ -85,12 +111,12 @@ def noise_augmentation(x, variance=0.001):
 def train_modal():
     dl_train, dl_test = get_dataloader()
 
-    model = MLP(640, 640, 640, 3).to(device)
+    model = MLP(640, 640, 640, 8).to(device)
     wandb.init(project='bridger', entity='ml_lab')
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     model.train()
-    for epoch in range(2 if DBG else 8):  # loop over the dataset multiple times
+    for epoch in range(2 if DBG else 30):  # loop over the dataset multiple times
         running_loss = 0.0
         correct = 0.0
         for i, data in enumerate(dl_train, 0):
